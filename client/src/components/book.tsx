8f02b6ed54fc419d436207ffa75860c032ba8e03
assac453 <ritterfrosch@yandex.ru>
2024-02-11 00:45:18 +0300

    feat: delete book

--- client/src/components/book.tsx
+++ client/src/components/book.tsx
@@ -7,10 +7,32 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import {BookType} from "@/types/book.ts";
+import {Button} from "@/components/ui/button.tsx";
+import {XIcon} from "lucide-react";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {BookService} from "@/services/BooksService.ts";
 
 export function Book({book}:{book: BookType}) {
+
+    const queryClient = useQueryClient()
+
+    const {mutate} = useMutation({
+        mutationKey:["delete", "book", book.id],
+        mutationFn: () => BookService.delete(book.id),
+        onSuccess: () => {queryClient.refetchQueries({
+            queryKey:["get", "book", book.id],
+        } )}
+
+    })
+    const deleteBook = () => {
+        mutate(book.id)
+    }
+
+
     return (
-        <Card>
+        <Card
+            className={"relative"}
+        >
             <CardHeader>
                 <CardTitle>{book?.title}</CardTitle>
                 <CardDescription>{book?.author}, {book?.yearOfPublishing}</CardDescription>
@@ -18,6 +40,13 @@ export function Book({book}:{book: BookType}) {
             <CardContent>
                 <p>Remain Count: {book?.remainCount}</p>
                 <p>Page Count: {book?.pageCount}</p>
+                <Button
+                    variant={"ghost"}
+                    className={"absolute top-4 right-4"}
+                    onClick={deleteBook}
+                >
+                    <XIcon />
+                </Button>
             </CardContent>
             <CardFooter>
                 <p>{book?.id}</p>
