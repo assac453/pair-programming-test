4d36575e03d0b3bcac944f1212c3154f32f9a505
assac453 <ritterfrosch@yandex.ru>
2024-02-10 23:41:57 +0300

    feat: base api setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { ThemeProvider } from "./components/theme-provider";
 import { Button } from "./components/ui/button";
+import {Book} from "@/components/book.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
+const queryClient = new QueryClient()
 function App() {
 	return (
-		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<div className="flex-1 flex flex-col items-center justify-center min-h-screen">
-				<Button variant={"destructive"}>Delete all world!</Button>
-			</div>
-		</ThemeProvider>
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<div>
+					<Book />
+				</div>
+			</ThemeProvider>
+		</QueryClientProvider>
 	);
 }
 
