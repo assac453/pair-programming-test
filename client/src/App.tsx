99a63b402c8acc5e828d876c5544ed18ef93e204
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-10 23:06:29 +0300

    style: shadcn/ui init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
+
 function App() {
-	return <div className="text-red-500 text-4xl">Hello world</div>;
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<div className="flex-1 flex flex-col items-center justify-center min-h-screen">
+				<Button variant={"destructive"}>Delete all world!</Button>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
