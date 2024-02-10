669b7e129240769722ad0a793eb3e7a836d93016
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-10 22:52:50 +0300

    feat: controller

--- backend/backend/src/main/java/com/assac453/backend/controller/BookController.java
+++ backend/backend/src/main/java/com/assac453/backend/controller/BookController.java
@@ -3,22 +3,38 @@ package com.assac453.backend.controller;
 import com.assac453.backend.dto.BookDto;
 import com.assac453.backend.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/book")
+@RequestMapping("/books")
 public class BookController {
     private final BookService service;
 
-    @PostMapping("/new")
-    public BookDto create(@RequestBody BookDto dto){
-        return service.create(dto);
+    @GetMapping("/")
+    public List<BookDto> getAll() {
+        return service.getAll();
     }
 
     @GetMapping("/{id}")
-    public BookDto delete(@PathVariable Integer id){
+    public BookDto getById(@PathVariable Integer id) {
+        return service.getById(id);
+    }
+
+    @PostMapping("/")
+    public BookDto create(@RequestBody BookDto dto) {
+        return service.create(dto);
+    }
+
+    @PutMapping("/{id}")
+    public BookDto update(@PathVariable Integer id, @RequestBody BookDto dto) {
+        return service.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public BookDto delete(@PathVariable Integer id) {
         return service.delete(id);
     }
 }
