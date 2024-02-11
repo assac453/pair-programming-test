eca9a2cc0a0f25e1a0c610be22ed38a17aadeae7
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-11 12:28:57 +0300

    feat: mode toggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import {ThemeProvider} from "./components/theme-provider";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {BooksList} from "@/components/books-list.tsx";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {ModeToggle} from "@/components/toggle-mode.tsx";
 
 const queryClient = new QueryClient()
 
@@ -10,7 +11,10 @@ function App() {
         <QueryClientProvider client={queryClient}>
             <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
                 <div className={"container flex flex-col my-8"}>
+                    <ModeToggle />
+                    <div className={"mt-10"}>
                     <BooksList/>
+                    </div>
                 </div>
             </ThemeProvider>
             <ReactQueryDevtools initialIsOpen={false} />
