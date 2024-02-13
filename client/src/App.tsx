db23476cc60bb8cc49d691f86e234d24fbe7dd40
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-13 22:08:15 +0300

    fix: jumping mode-toggle

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,28 @@
-import {ThemeProvider} from "./components/theme-provider";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {BooksList} from "@/components/books-list.tsx";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import {ModeToggle} from "@/components/toggle-mode.tsx";
+import { ThemeProvider } from "./components/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BooksList } from "@/components/books-list.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ModeToggle } from "@/components/toggle-mode.tsx";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-    return (
-        <QueryClientProvider client={queryClient}>
-            <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-                <div className={"container flex flex-col my-8"}>
-                    <ModeToggle />
-                    <div className={"mt-10"}>
-                    <BooksList/>
-                    </div>
-                </div>
-            </ThemeProvider>
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+				<div className={"container flex flex-col my-8"}>
+					<div className="flex justify-end">
+						<ModeToggle />
+					</div>
+
+					<div className={"mt-10"}>
+						<BooksList />
+					</div>
+				</div>
+			</ThemeProvider>
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
 
 export default App;
