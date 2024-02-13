e56a7db062a78b91df81316e8cddf51a48af085d
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-13 22:57:41 +0300

    feat: BookTypeWithoutId

--- client/src/types/book.ts
+++ client/src/types/book.ts
@@ -1,8 +1,10 @@
 export type BookType = {
-    id: number,
-    author: string,
-    title: string,
-    yearOfPublishing: string,
-    pageCount: number,
-    remainCount: number
-}
+	id: number;
+	author: string;
+	title: string;
+	yearOfPublishing: string;
+	pageCount: number;
+	remainCount: number;
+};
+
+export type BookTypeWithoutId = Omit<BookType, "id">;
